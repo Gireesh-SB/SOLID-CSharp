392fb2ac0a25633b0062d21a7406980fd7a7a986
Gireesh SB <gireeshsb@live.com>
2018-07-27 01:02:47 +0530

    single responsibility principle

---
+++ src/SOLID/SingleResponsibility/Problem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleResponsibility.Problem
+{
+    class Machine
+    {
+        public void Start()
+        {
+            try
+            {
+                //Do Something
+            }
+            catch (Exception ex)
+            {
+                string path = "";
+                System.IO.File.WriteAllText(path, ex.ToString());
+            }
+        }
+    }
+}
---
+++ src/SOLID/SingleResponsibility/Solution.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleResponsibility.Solution
+{
+    class Machine
+    {
+        public void Start()
+        {
+            try
+            {
+                //Do Something
+            }
+            catch (Exception ex)
+            {
+                new ExceptionHandler().Handle(ex);
+                throw;
+            }
+        }
+    }
+
+    class ExceptionHandler
+    {
+        string path = "";
+        public void Handle(Exception ex)
+        {
+            System.IO.File.WriteAllText(path, ex.ToString());
+        }
+    }
+}
